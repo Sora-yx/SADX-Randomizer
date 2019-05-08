2e08f3942b5445e40f0c68ec2696e962da352719
Sora <sora.x@hotmail.fr>
2019-05-08 15:05:01 +0200

    Banned Sand Hill because of crash Removed Sky Deck as Tails from Regular Level.

--- sadx-randomizer/RandomHelpers.cpp
+++ sadx-randomizer/RandomHelpers.cpp
@@ -13,15 +13,15 @@ extern "C"
 {
 	//Initialize Banned stage impossible to beat
 	int bannedLevelsGamma[8] = { 3, 15, 16, 18, 20, 21, 23, 38 };
-	int bannedLevelsBig[2] = { 8, 22 };
+	int bannedLevelsBig[3] = { 8, 22, 38 };
 	int bannedLevelsAmy[1] = { 38 };
 
 	//Initiliaze banned regular stage if option is activated
 	int bannedRegularSonic[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 20, 22 };
-	int bannedRegularTails[4] = { 4, 6, 20, 21 };
+	int bannedRegularTails[4] = { 4, 20, 21, 38 };
 	int bannedRegularKnuckles[1] = { 16 };
 	int bannedRegularAmy[3] = { 12, 23, 38 };
-	int bannedRegularBig[3] = { 8, 12, 22 };
+	int bannedRegularBig[4] = { 8, 12, 22, 38 };
 	int bannedRegularGamma[9] = { 1, 3, 15, 16, 18, 20, 21, 23, 38 };
 
 
@@ -29,7 +29,7 @@ extern "C"
 		if (RNGCharacters == true) {
 			do {
 				CurrentCharacter = character[rand() % 6];
-			} while (CurrentCharacter == Characters_Gamma && isValueInArray(bannedLevelsGamma, CurrentLevel, 8) || CurrentCharacter == Characters_Big && isValueInArray(bannedLevelsBig, CurrentLevel, 2 || CurrentCharacter == Characters_Amy && isValueInArray(bannedLevelsAmy, CurrentLevel, 1)));
+			} while (CurrentCharacter == Characters_Gamma && isValueInArray(bannedLevelsGamma, CurrentLevel, 8) || CurrentCharacter == Characters_Big && isValueInArray(bannedLevelsBig, CurrentLevel, 3 || CurrentCharacter == Characters_Amy && isValueInArray(bannedLevelsAmy, CurrentLevel, 1)));
 		}
 	}
 
@@ -67,7 +67,7 @@ extern "C"
 					do {
 						CurrentLevel = level[rand() % 18];
 						CurrentAct = 0;
-					} while (isValueInArray(bannedRegularBig, CurrentLevel, 3));
+					} while (isValueInArray(bannedRegularBig, CurrentLevel, 4));
 					break;
 				case Characters_Gamma:
 					do {
