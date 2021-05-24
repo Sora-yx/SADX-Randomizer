c7a02133cc016a672669149aa8505a84c090e3ac
Sora <sora.x@hotmail.fr>
2021-05-24 19:32:31 +0200

    headers cleanup

--- sadx-randomizer/SADXFunctions.h
+++ sadx-randomizer/SADXFunctions.h
@@ -1,102 +1,44 @@
 #pragma once
 
-FunctionPointer(void, StartCutsceneFlag, (int event), 0x630590);
-FunctionPointer(void, CameraSetEventCamera, (short a1, unsigned char a2), 0x437bf0);
-
 FunctionPointer(void, StartCutscene, (long flag), 0x4136e0);
-FunctionPointer(void, StartCutscene2, (int flag), 0x52e6c0);
-
-
 FunctionPointer(void, KillPlayer, (uint8_t ID), 0x440CD0);
-VoidFunc(PauseQuitDisplay, 0x415450);
-VoidFunc(E101ShootThing, 0x567ac0);
 VoidFunc(Tails_CheckRaceResult, 0x461560);
-VoidFunc(EndLevelStuff, 0x4179bc);
-
 VoidFunc(SomethingAboutFlag, 0x42f5d0);
-
-ObjectFunc(Result_Score, 0x4141f0);
-
 VoidFunc(ScreenTextureVer, 0x432ea0);
-VoidFunc(SomethingAboutSave, 0x42d2f0);
 VoidFunc(FUN_00412ad0, 0x412ad0);
 VoidFunc(FUN_00425b00, 0x425b00);
-
-VoidFunc(SetupZero, 0x4b3eb0);
 VoidFunc(CheckLoadZero, 0x486a40);
 
 VoidFunc(EggViperCutscene, 0x431430);
-ObjectFunc(Load_Chaos4, 0x552bb0);
-VoidFunc(DeleteBubbleChaos, 0x7ae5b0);
-VoidFunc(ChaosUnderLightDelete, 0x7ad2f0);
-VoidFunc(FUN_007a1580, 0x7a1580);
 
-VoidFunc(LoadE101, 0x568090);
 ObjectFunc(InvisibleWallCasino, 0x5d03a0);
 ObjectFunc(SDCannon, 0x5f84e0);
 ObjectFunc(SDCannonS1, 0x5f9710);
 ObjectFunc(SDCannonS2, 0x5f8e00);
-ObjectFunc(TikalHintEmerald, 0x7a9020);
 
 FunctionPointer(int, PerformWhistle, (EntityData1* a1, EntityData2* a2, CharObj2* a3, int flag), 0x442570);
-FunctionPointer(void, GiveSpeedShoes, (EntityData1* chara), 0x441df0);
-
-
 FunctionPointer(void, BossHP_Stuff, (int x, int y, int hp), 0x4b3cc0);
-FunctionPointer(char, CameraReleaseEventCam, (), 0x436140);
-FunctionPointer(char, BigResetCamera, (), 0x48CE10);
-FunctionPointer(void, SetRNGAmyDoor, (), 0x5b06f);
-VoidFunc(Reset_HotShelterSwitch, 0x59a1b0);
-VoidFunc(LoadSplines, 0x49c1a0);
 
 VoidFunc(FUN_0042ce20, 0x42ce20); //Called with Delete Tails AI
-
-VoidFunc(CheckTC, 0x79a8e0);
-
-VoidFunc(CutsceneFlagData, 0x431430);
-VoidFunc(SkyDeckResetValue, 0x5ec790);
-VoidFunc(SomethingAboutMK2, 0x56b0c0);
-VoidFunc(SomethingAboutZeroBoss, 0x5875d0);
-VoidFunc(FUN_0040bda0, 0x40bda0);
-VoidFunc(FUN_0040be30, 0x40be30);
 VoidFunc(GetLevelCutscene, 0x413b20);
-FunctionPointer(int, CapsuleCheck, (), 0x46a820);
-
 VoidFunc(Knuckles_SetRNG, 0x475840);
 VoidFunc(PauseQuitThing2, 0x413f70);
-VoidFunc(FUN_00424830, 0x424830);
-FunctionPointer(void, sNumberForGame, (unsigned short player), 0x414470);
-
 
 //Cutscenes stuff
 FunctionPointer(void, EV_Wait, (int time), 0x4314d0);
-FunctionPointer(void, StopMusicAndLoadNextMenu, (int a1), 0x505B40);
 FunctionPointer(void, SeqRun, (), 0x412AC0);
 FunctionPointer(void, SeqSetPlayer, (__int16 character), 0x413380);
 FunctionPointer(void, SeqSetSection, (__int16 sequence), 0x4133E0);
-FunctionPointer(void, RunSceneSelect, (), 0x413870);
 ObjectFunc(SeqTaskFadeIn, 0x412fe0);
 FunctionPointer(void, EventCutscene_Load2, (long flag), 0x42fa30);
-FunctionPointer(void, EV_InitPlayer, (int player), 0x431780);
 VoidFunc(EventCutscene_Exec2, 0x431430);
 
 //Chao Stuff
-FunctionPointer(int, Chao_Animation, (ObjectMaster * a1, int a2), 0x734F00);
-FunctionPointer(bool, Chao_FinishedAnimation, (ObjectMaster* a1), 0x735040);
-FunctionPointer(EntityData1*, SpawnAnimal, (int unknown, float x, float y, float z), 0x4BE610);
-FunctionPointer(int, WhistleFunction, (EntityData1* a1, EntityData2* a2, CharObj2* a3, int flag), 0x442570);
-FunctionPointer(int, BigWhistle, (int a3, int a4, int a5, int a6), 0x423BE0);
 FunctionPointer(long, Chao_Cry, (ObjectMaster* Chao), 0x75b6c0);
 FunctionPointer(long, Chao_Pleasure, (ObjectMaster* Chao), 0x75db80);
 FunctionPointer(void, Chao_SetBehavior, (ObjectMaster* Chao, long* a2), 0x71EF10);
 
-
-
-VoidFunc(PushMSIcon, 0x425e6d);
-VoidFunc(CharSel_LoadThing, 0x512bc0);
 FunctionPointer(ObjectMaster*, CheckLoadBird, (), 0x4C6820);
-VoidFunc(SomethingAboutWater2, 0x4497b6);
-
 
 ObjectFunc(EggHornet_Load, 0x572230);
 VoidFunc(sub_5919E0, 0x5919E0);
--- sadx-randomizer/SADXVariables.h
+++ sadx-randomizer/SADXVariables.h
@@ -1,14 +1,7 @@
 #pragma once
 
 DataPointer(char, TCQuit, 0x3c5d518);
-DataPointer(char, EventTailsData, 0x3B18809);
-DataPointer(char, SonicChaos4Flag, 0x3B188B9);
-DataPointer(char, AdventureDataChaos4, 0x3B183AA);
-DataPointer(char, AdventureDataChaos4Post, 0x3B183AE);
 
-DataPointer(char, ChaosAdventureData, 0x3B1860A);
-DataPointer(char, TailsAdventureData, 0x3B1860E);
-DataPointer(char, CasinoSwitch, 0x3c5b37c);
 DataPointer(char, KnuxCheck, 0x3c52c04);
 DataPointer(char, KnuxEmerald2, 0x3C52B48);
 DataPointer(char, KnuxCheck2, 0x3c52bd8);
@@ -22,7 +15,6 @@ DataPointer(char, SecretWaterSwitch, 0x3C5B37E);
 
 DataPointer(char, FlagAutoPilotCart, 0x3D08E00);
 
-DataPointer(char, TornadoThing, 0x3c5d670);
 
 DataPointer(char, TitleCardStuff, 0x3c53abc);
 DataPointer(char, TitleCardStuff2, 0x3c53abd);
@@ -37,24 +29,17 @@ DataPointer(signed char, signed_char_ccsi_mode, 0x3c4abb4);
 
 DataPointer(char, BossHP, 0x3c58148);
 DataPointer(char, BossHP2, 0x3C58150);
-DataPointer(float, Life_Max, 0x3c58158);
-DataPointer(char, Emblem, 0x974AE0);
+
 DataPointer(unsigned char, ValueMenu, 0x3B2C5F8);
 DataPointer(unsigned char, SelectedCharacter, 0x3B2A2FD);
 DataPointer(char, RNGDoor, 0x3C7457C);
 
-DataPointer(float, EggViperFloatThing, 0x3c6e330);
-DataPointer(char, InitUpgrade, 0x3c5a954);
 DataPointer(int, BigWeight, 0x3C52510);
 
-
-DataPointer(ObjectMaster*, CurrentMenuObjectMaster_Maybe, 0x3C5E8D0);
 DataPointer(float, flt_3B18244, 0x3B18244);
-DataPointer(int, dword_3B28114, 0x3B28114);
 DataPointer(int, dword_3B2A304, 0x3B2A304);
 
 DataPointer(char, EmeraldKnuxCheck, 0x3c52c04);
-
 DataPointer(char, SomethingAboutCredit, 0x3c83054);
 
 DataPointer(NJS_VECTOR, stru_3B2C6DC, 0x3B2C6DC);
